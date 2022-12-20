ef0589014b97618d7e99387421cc35889efb2749
SmartKid <103258755+TheeSmartKid@users.noreply.github.com>
2022-12-20 21:58:41 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "In India, floods mostly affect people living in _______ areas.",
+    answer: "Low-laying",
+    options: [
+      "Low-laying",
+      "Rural",
+      "High-laying",
+      "Urban"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which is not a natural disaster??",
+    answer: "Lightning",
+    options: [
+      "Earthquakes",
+      "Cyclones",
+      "Lightning",
+      "Floods"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which Of These Is Not A Earthquake Zone?",
+    answer: "Jharkhand",
+    options: [
+      "Bihar",
+      "Jharkhand",
+      "Latur",
+      "Uri"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which Of These Is Not True About Cyclones?",
+    answer: "The formation is more common in temperate regions than in tropical",
+    options: [
+      "A cyclone is an intense low pressure centre into which winds converge",
+      "Cyclones are also called hurricanes and typhons",
+      "The coastal states of India such as West Bengal,Odisha,Andhra Pradesh,Telangana and Tamil Nadu are vulnerable to cyclones originationg from Bay Of Bengal",
+      "The formation is more common in temperate regions than in tropical"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which Of The Following is NOT TRUE Regarding Chapter Natural Disaters?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "Coastal area are prone to cyclones",
+      "The Himalyan region is prone to drought",
+      "Earthquake occur where earth's plate rub against each other",
+      "We schould plant as many trees as possible to prevent droughts"
+    ]
+  },
+];
